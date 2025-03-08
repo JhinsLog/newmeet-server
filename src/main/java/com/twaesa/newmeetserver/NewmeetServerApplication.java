aa80c1f052ce9d03871fa9e7738bcb7963703291
Jhin <joojh501@gmail.com>
2025-03-08 18:42:26 +0900

    Init Project

---
+++ src/main/java/com/twaesa/newmeetserver/NewmeetServerApplication.java
@@ -0,0 +1,13 @@
+package com.twaesa.newmeetserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewmeetServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewmeetServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/twaesa/newmeetserver/NewmeetServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twaesa.newmeetserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewmeetServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
